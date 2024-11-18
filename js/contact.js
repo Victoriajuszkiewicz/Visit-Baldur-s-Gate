d32dfb630b46d36bf1cae5f719c32d53eba2fe95
Victoria Juszkiewicz (Tallaght TC Student) <wanderer.foxy@gmail.com>
2024-11-18 14:40:48 +0000

    form contact us works

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,20 @@
 function handleForm(event){
-    const form = document.querySelector(".contact-form-container");
-
-    console.log("button clicked here!!!")
+    //prevents refresh
+    event.preventDefault()
+    //receive uinfo from form
+    const form=event.target;
+    const formInfo={
+        nameonForm: form.querySelector("#name").value,
+        emailonForm: form.querySelector("#email").value,
+    pigeonNo: form.querySelector("#pigeonNumber").value,
+    };
+const alert= document.getElementById("alert-box");
+ alert.classList.remove("d-none");
+setTimeout(()=>{
+    location.reload()
+}, 2000);
+//clean the form
+    console.log("button clicked here!!!", formInfo )
+   //clean the form and cart!!!
+   form.reset();
 }
